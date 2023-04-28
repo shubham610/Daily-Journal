596d4a815c01e5bd1cd8479a58d36651cc0ebd24
shubham bansal <shubh.6102001@gmail.com>
2023-04-29 00:19:14 +0530

    combined 2 commmits
    added contact us mailer
    
    removed package-lock from node modules

--- app.js
+++ app.js
@@ -5,7 +5,20 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose=require("mongoose");
 const _=require("lodash");
+const nodemailer=require("nodemailer");
+require('dotenv').config();
+console.log(process.env.USER);
 // const posts=[];
+
+var transporter=nodemailer.createTransport({
+  service:'gmail',
+  auth:{
+    user:process.env.USER,
+    pass:process.env.PASS
+  }
+});
+
+
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
@@ -37,6 +50,32 @@ post.save();
 //   posts.push(post);
   res.redirect("/");
 });
+
+app.post("/contact",function(req,res){
+  var name=req.body.username;
+  var gmail=req.body.email;
+  var subject=req.body.subject;
+  var message=req.body.message;
+  console.log(name,gmail,subject,message);
+
+  var mailOptions={
+    from:gmail,
+    to:process.env.receiverMail,
+    subject:subject,
+    text:message
+  };
+
+  transporter.sendMail(mailOptions, function(error, info){
+    if (error) {
+      console.log(error);
+    } else {
+      console.log('Email sent: ' + info.response);
+    }
+  });
+
+  res.redirect("/contact");
+});
+
 app.get("/",function(req,res){
   Post.find({}, function(err, posts){
      res.render("home", {
